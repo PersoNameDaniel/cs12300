f6c96c6f4bfacafc5c380e8851b67b170ab0b5b7
PersoNameDaniel <dmolsson@outlook.com>
2023-04-04 10:44:17 -0500

    Lab 7.27 Created
    Template copied into file, nothing done.

---
+++ Chapter 7/Lab 7.27.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class LabProgram {
+   
+   /* Define your method here */
+   
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      Random rand = new Random(2); // Seed used in develop mode
+      // Add more variables as needed
+
+      /* Type your code here. */
+   }
+}
