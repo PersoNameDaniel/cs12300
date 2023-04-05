62de35075ad7b940cc93674709348bda6fc0e855
PersoNameDaniel <dmolsson@outlook.com>
2023-04-05 13:48:39 -0500

    Lab 7.30 done

--- Chapter 7/Lab 7.30.java
+++ Chapter 7/Lab 7.30.java
@@ -3,7 +3,28 @@ import java.util.Scanner;
 public class LabProgram {
    
    public static int fibonacci(int n) {
-      /* Type your code here. */       
+      if (n < 0) {
+         return -1;
+      }
+      else if (n == 0) {
+         return 0;
+      }
+      else if (n == 1) {
+         return 1;
+      }
+      else {
+         int prev1 = 1;
+         int prev2 = 0;
+         int current = 0;
+         
+         for (int i = 2; i <= n; i++) {
+            current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+         }
+         
+         return current;
+      }
    }
    
    public static void main(String[] args) {
