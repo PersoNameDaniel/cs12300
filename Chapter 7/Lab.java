4518ec0673aac13ae664cf5640812272d49adce4
PersoNameDaniel <dmolsson@outlook.com>
2023-05-03 10:51:34 -0500

    file returns output, changed name
    Changed name to be able to run the file locally.
    The file runs and returns an output, but is incorrect.

---
+++ Chapter 7/Lab.java
@@ -0,0 +1,55 @@
+import java.util.Scanner; 
+import java.lang.Math;
+
+public class Lab {
+   // All x, y, coordinates are in miles from the origin 0, 0. 
+   public static int pickupMinutes(int userX, int userY, int firstDriverX, int firstDriverY, int secondDriverX, int secondDriverY, int thirdDriverX, int thirdDriverY) { 
+      
+      int firstDriverMinutes;
+      int secondDriverMinutes;
+      int thirdDriverMinutes;
+
+      // getting distances of drivers
+      firstDriverMinutes = distance(userX, userY, firstDriverX, firstDriverY);
+      secondDriverMinutes = distance(userX, userY, secondDriverX, secondDriverY);
+      thirdDriverMinutes = distance(userX, userY, thirdDriverX, thirdDriverY);
+
+      // finding closest driver
+      if ((firstDriverMinutes < secondDriverMinutes) && (firstDriverMinutes < thirdDriverMinutes)) {
+         return firstDriverMinutes;
+      }
+      else if (secondDriverMinutes < thirdDriverMinutes) {
+         return secondDriverMinutes;
+      }
+      else {
+         return thirdDriverMinutes;
+      }
+   } 
+
+   public static int distance(int x1, int y1, int x2, int y2) {
+      int distanceBetweenPoints;
+
+      distanceBetweenPoints = ((Math.abs(x1 - x2)) + (Math.abs(y1 - y2)));
+
+      return distanceBetweenPoints;
+   }
+   
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in); 
+      int userX, userY; 
+      int d1X, d1Y; 
+      int d2X, d2Y; 
+      int d3X, d3Y; 
+      
+      userX = scnr.nextInt(); 
+      userY = scnr.nextInt(); 
+      d1X = scnr.nextInt(); 
+      d1Y = scnr.nextInt(); 
+      d2X = scnr.nextInt(); 
+      d2Y = scnr.nextInt(); 
+      d3X = scnr.nextInt(); 
+      d3Y = scnr.nextInt(); 
+      
+      System.out.println(pickupMinutes(userX, userY, d1X, d1Y, d2X, d2Y, d3X, d3Y)); 
+   }
+}
