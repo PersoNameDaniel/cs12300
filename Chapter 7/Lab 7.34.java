a43963eb278e0402201580f215b610638e91cada
PersoNameDaniel <dmolsson@outlook.com>
2023-05-03 11:03:50 -0500

    renamed last file. created new file
    Renamed last lab file for archive.
    Created new file and copied starting template.

---
+++ Chapter 7/Lab 7.34.java
@@ -0,0 +1,62 @@
+import java.util.Scanner; 
+import java.lang.Math;
+
+public class LabProgram {
+   // All x, y, coordinates are in miles from the origin 0, 0. 
+   public static int pickupMinutes(int userX, int userY, int firstDriverX, int firstDriverY, int secondDriverX, int secondDriverY, int thirdDriverX, int thirdDriverY) { 
+      
+      int firstDriverMinutes;
+      int secondDriverMinutes;
+      int thirdDriverMinutes;
+
+      // getting distances of drivers
+      firstDriverMinutes = driveTime(userX, userY, firstDriverX, firstDriverY);
+      secondDriverMinutes = driveTime(userX, userY, secondDriverX, secondDriverY);
+      thirdDriverMinutes = driveTime(userX, userY, thirdDriverX, thirdDriverY);
+
+      // finding closest driver
+      return smallestDistance(firstDriverMinutes, secondDriverMinutes, thirdDriverMinutes);
+   } 
+
+   public static int driveTime(int x1, int y1, int x2, int y2) {
+      int distanceBetweenPoints;
+      int timeToDrive;
+
+      distanceBetweenPoints = ((Math.abs(x1 - x2)) + (Math.abs(y1 - y2)));
+
+      timeToDrive = distanceBetweenPoints * 2;
+
+      return timeToDrive;
+   }
+
+   public static int smallestDistance(int firstValue, int secondValue, int thirdValue) {
+      if ((firstValue < secondValue) && (firstValue < thirdValue)) {
+         return firstValue;
+      }
+      else if (secondValue < thirdValue) {
+         return secondValue;
+      }
+      else {
+         return thirdValue;
+      }
+   }
+   
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in); 
+      int userX, userY; 
+      int d1X, d1Y; 
+      int d2X, d2Y; 
+      int d3X, d3Y; 
+      
+      userX = scnr.nextInt(); 
+      userY = scnr.nextInt(); 
+      d1X = scnr.nextInt(); 
+      d1Y = scnr.nextInt(); 
+      d2X = scnr.nextInt(); 
+      d2Y = scnr.nextInt(); 
+      d3X = scnr.nextInt(); 
+      d3Y = scnr.nextInt(); 
+      
+      System.out.println(pickupMinutes(userX, userY, d1X, d1Y, d2X, d2Y, d3X, d3Y)); 
+   }
+}
